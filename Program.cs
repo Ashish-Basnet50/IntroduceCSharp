9471a1af1f15bd27efd6825f861386577d240b4b
Ashish Basnet <ashishbasnet32@gmail.com>
2022-01-07 06:34:45 +0545

    TestingCsharp

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IntroductionToCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Built Data Types Calculated In Min Max
+            /*
+               int i= 0;
+               Console.WriteLine("Min={0}",int.MinValue);
+               Console.WriteLine("Max={0}", int.MaxValue);
+            */
+
+            //Escape Squence
+            /*
+            string Name = "One\nTwo\nThree";
+            Console.WriteLine(Name);
+            */
+        }
+    }
+}
