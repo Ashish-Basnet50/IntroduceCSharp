120ea4133b5aee3c8fc38e5a6cf6559c61c82e8c
Ashish Basnet <ashishbasnet32@gmail.com>
2022-01-25 07:15:41 +0545

    Practise C-sharp

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace IntroductionToCSharp
             Console.WriteLine(Name);
             */
 
-            bool? Areyoustudent = null;
+            /*bool? Areyoustudent = null;
             if (Areyoustudent == true)
             {
                 Console.WriteLine("Yes");
@@ -32,6 +32,43 @@ namespace IntroductionToCSharp
             {
                 Console.WriteLine("Not Selected");
             }
+            */
+
+
+            //Null Coalescing Operator
+
+            /*
+             int? TicketonSales = null;
+            int AvailableTickets = TicketonSales ?? 0;
+            Console.WriteLine("AvailableTickets ={0}", AvailableTickets);
+            */
+
+            //parse method
+            /*
+            string strnumber = "100";
+
+            int i = int.Parse(strnumber);
+
+            Console.WriteLine(i);
+            */
+
+            //try parse method
+
+            String strnumber = "100ST";
+
+            int Result = 0;
+
+            bool IsconversionSucessful = int.TryParse(strnumber,out Result);
+
+            if (IsconversionSucessful)
+            {
+                Console.WriteLine(Result);
+            }
+            else
+            {
+                Console.WriteLine("Please Enter Valid Number");
+            }
+
         }
     }
 }
